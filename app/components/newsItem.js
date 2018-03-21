ecc60ef996673d1730ded800fdce6806ac86ba99
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 18:19:04 +0000

    feat(theme): add styles to the news item component

--- app/components/newsItem.js
+++ app/components/newsItem.js
@@ -7,10 +7,12 @@ import PropTypes from 'prop-types';
 import '../styles/components/_newsItem.scss';
 
 const NewsItem = (props) => (
-  <div className="list-item">
-    <p>{props.news.title}</p>
-    <p>{props.news.url}</p>
-    <p>{props.news.date}</p>
+  <div className="news-item">
+    <a href={props.news.url} target="_blank" className="news-item__title">{props.news.title}</a>
+    <p className="news-item__info">
+      <span>{props.news.newsSourceCategory} - </span>
+      <span>{props.news.date}</span>
+    </p>
   </div>
 );
 
