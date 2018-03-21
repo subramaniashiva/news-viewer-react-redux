f8ca7008f0cfe4367373343cca1e7b1e057e9af1
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 22:21:09 +0000

    fix(layout): Fix the layout for mobile screens

--- app/components/header.js
+++ app/components/header.js
@@ -6,8 +6,10 @@ import React from 'react';
 import '../styles/components/_header.scss';
 
 const Header = () => (
-  <div className='header'>
-    <h3>Zone - News Viewer</h3>
+  <div className="header-container">
+    <div className='header'>
+      <h3>Zone - News Viewer</h3>
+    </div>
   </div>
 )
 
