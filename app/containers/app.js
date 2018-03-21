470e826b1a8727885ce5c87ddf278a6e35439014
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 00:20:55 +0000

    feat(api-call): Update main app container to hold list component. Update webpack to have babel polyfill for generators

--- app/containers/app.js
+++ app/containers/app.js
@@ -3,24 +3,41 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Loading from '../components/loading';
+import NewsList from '../components/newsList';
+
+import { loadNewsList } from '../redux/reducers/newsList';
 
 export class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(loadNewsList());
+  }
   render () {
     return (
       <div>
-        {!this.props.loading ? (<div>React app</div>) : (<div><Loading /></div>)}
+        {!this.props.loading ?
+          (
+            <div className="container">
+              <NewsList list={this.props.newsList}></NewsList>
+            </div>
+          ) :
+          (
+            <Loading />
+          )
+        }
       </div>
     )
   }
 }
 
 App.propTypes = {
-  loading: PropTypes.bool
+  loading: PropTypes.bool,
+  newsList: PropTypes.array
 }
 
 const mapStateToProps = (state) => {
   return {
-    loading: state.loading
+    loading: state.loading,
+    newsList: state.newsList
   }
 }
 
--- app/redux/store.js
+++ app/redux/store.js
@@ -2,8 +2,15 @@
   Redux store file.
   Combines the root reducer, thunk middleware (for API requests)
 */
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './reducers/rootReducer';
+import sagas from '../sagas/';
 
-export default createStore(rootReducer, {});
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+// mount it on the Store
+export default createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(sagas);
