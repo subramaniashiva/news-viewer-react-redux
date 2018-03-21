7f46197c2b1d2deb7beb19f03da25157fc7a4412
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 22:00:57 +0000

    feat(tests): Safeguard against null value for props for the components

--- app/components/newsItem.js
+++ app/components/newsItem.js
@@ -8,15 +8,18 @@ import {getDateString} from '../utils/helper';
 
 import '../styles/components/_newsItem.scss';
 
-const NewsItem = (props) => (
-  <div className="news-item">
-    <a href={props.news.url} target="_blank" className="news-item__title">{props.news.title}</a>
-    <p className="news-item__info">
-      <span>{props.news.newsSourceCategory} - </span>
-      <span>{getDateString(props.news.date)}</span>
-    </p>
-  </div>
-);
+const NewsItem = (props) => {
+  {
+    return props.news ? (
+    <div className="news-item">
+      <a href={props.news.url} target="_blank" className="news-item__title">{props.news.title}</a>
+      <p className="news-item__info">
+        <span>{props.news.newsSourceCategory} - </span>
+        <span>{getDateString(props.news.date)}</span>
+      </p>
+    </div>): null
+  }
+};
 
 NewsItem.propTypes = {
   news: PropTypes.object
