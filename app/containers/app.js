4366109d69aa16d6e7bfafb49e24fe013292a949
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 18:20:18 +0000

    feat(theme): add heder to main app component and update styles

--- app/containers/app.js
+++ app/containers/app.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import Loading from '../components/loading';
 import NewsList from '../components/newsList';
 import FilterNewsSource from '../components/filterNewsSource';
+import Header from '../components/header';
 
 import { loadNewsList } from '../redux/reducers/newsList';
 import { setCurrentSource } from '../redux/reducers/currentSource';
@@ -24,14 +25,23 @@ export class App extends Component {
   }
   render () {
     let newsResourcesList = Object.values(NEWS_RESOURCES);
+    const newsListFiltered = this.getNewsList();
     return (
       <div>
+        <Header />
         {!this.props.loading ?
           (
             <div className="container">
-              <FilterNewsSource newsSourceList={newsResourcesList} selectedSource={this.props.currentSource}
-                handleFilterChange={this.handleFilterChange.bind(this)}></FilterNewsSource>
-              <NewsList list={this.getNewsList()}></NewsList>
+              <div className="filter-container">
+                <FilterNewsSource newsSourceList={newsResourcesList} selectedSource={this.props.currentSource}
+                  handleFilterChange={this.handleFilterChange.bind(this)}></FilterNewsSource>
+              </div>
+              {
+                newsListFiltered && newsListFiltered.length ?
+                <div className="list-container">
+                  <NewsList list={newsListFiltered}></NewsList>
+                </div> : <p>Sorry! No news found</p>
+              }
             </div>
           ) :
           (
