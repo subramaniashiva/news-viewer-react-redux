cf1d8926251313fae01b1f9e4c99788617b61eff
subramaniashiva <vengaishiva@gmail.com>
2018-03-21 18:17:29 +0000

    feat(theme): add header component to the app

---
+++ app/components/header.js
@@ -0,0 +1,14 @@
+/*
+  Loading component - stateless
+*/
+import React from 'react';
+
+import '../styles/components/_header.scss';
+
+const Header = () => (
+  <div className='header'>
+    <h3>Zone - News Viewer</h3>
+  </div>
+)
+
+export default Header;
